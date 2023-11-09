26d49925298a3be0c88dda2a1c3b3516407b879d
jotano <jotanosanm@educacion.navarra.es>
2023-11-09 16:51:22 +0100

    Ejercicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,7 @@
+nombre = input("Escribe tu nombre: ")
+edad = input("Introduce tu edad: ")
+direccion = input("Introduce tu dirección: ")
+telefono = input("Escribe tu número de teléfono: ")
+diccionario = {"nombre": nombre, "edad": edad, "direccion": direccion, "telefono": telefono}
+print(nombre,"tiene",edad,"años, vive en",direccion,"y su número de teléfono es",telefono)
+input("Pulsa enter para salir")
